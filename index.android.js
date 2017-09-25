5b3e6bf93576193c6ae2cd6eb61305e0678ad5e6
Alertas <justinaszailskas@gmail.com>
2017-09-25 22:24:07 +0300

    Rugsejo 25 paskaitos pavyzdys

---
+++ index.android.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Header from './src/components/component';
+import NoHeader from './src/components/component2';
+
+class rugsejo25pvz extends Component {
+  render() {
+    if (3>4){ 
+      return <Header />} 
+    else return <NoHeader />
+  } 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('rugsejo25pvz', () => rugsejo25pvz);
---
+++ src/components/component2.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const NoHeader = () => {
+	return <Text>Cia nera antraste</Text>
+}
+
+export default NoHeader;
